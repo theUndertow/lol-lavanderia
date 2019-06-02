7bc8e50798874e2492f76af22ff4c777e7b000ad
theundertow <Marco_Olivette21@outlook.com.br>
2019-06-02 00:18:04 -0300

    Criação das telas - Cadastro funcionario - Cadastro roupa - Cadastro Tipo de roupa

--- src/main/java/com/dac/lol/manbe/CadastroManbe.java
+++ src/main/java/com/dac/lol/manbe/CadastroManbe.java
@@ -11,10 +11,7 @@ import com.dac.lol.model.*;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import sun.security.provider.MD5;
 
 /**
  *
